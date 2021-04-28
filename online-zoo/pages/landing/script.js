668e813e0737ff023da6b9f520f8216c2a9bffe7
katiandrews <andryushina.eka@gmail.com>
2021-04-28 17:22:41 +0200

    feat: add popup

--- online-zoo/pages/landing/script.js
+++ online-zoo/pages/landing/script.js
@@ -81,6 +81,49 @@ document.querySelector('.map-pin-panda').addEventListener('click', function(even
 
 // ====================
 // show pop-up on page
+const leaveFeedbackBtn = document.querySelector('.testimonials-button');
+const popup = document.querySelector('.pop-up');
+
+function showPopup() {
+    popup.classList.add('pop-up-open');
+}
+
+leaveFeedbackBtn.addEventListener('click', function(event) {
+  showPopup();
+  event.preventDefault();
+});
+
+// close popup, when clicking outside its content
+popup.addEventListener('mousedown', function(event) {
+  const popupContent = document.querySelector('.pop-up_content')
+  if(event.target !== popupContent && !popupContent.contains(event.target)) {
+    popup.classList.remove('pop-up-open');
+  }
+})
+
+// check if all inputs are filled and then make button enable;
+function checkValidation() {
+  const buttonForm = document.querySelector('.button_form')
+  const inputs = document.querySelectorAll('.input');
+  const textArea = document.querySelector('.textarea');
+  for (let i = 0; i < inputs.length; i += 1) {
+    if (inputs[i].value === '') {
+      buttonForm.classList.add('button_disabled');
+      buttonForm.disabled = true;
+      return;
+    }
+    if (textArea.value !== '') {
+      buttonForm.classList.remove('button_disabled');
+      buttonForm.disabled = false;
+    } else {
+      buttonForm.classList.add('button_disabled');
+      buttonForm.disabled = true;
+    }
+  }
+}
+
+popup.addEventListener('keyup', checkValidation);
+
 
 
 
