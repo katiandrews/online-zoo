1030fd5419a83249411214c07653fafe1086073d
katiandrews <andryushina.eka@gmail.com>
2021-04-29 21:34:05 +0200

    feat: add ability to open and close accordeon

--- online-zoo/pages/zoos translation/changeVideo.js
+++ online-zoo/pages/zoos translation/changeVideo.js
@@ -13,3 +13,25 @@ for (let i = 0; i < layers.length; i++) {
     changeVideo(smallVideo);
   })
 };
+
+// ==============
+// open and close accordeon
+const accordeons = document.querySelectorAll('.accordeon-item');
+for (let i = 0; i < accordeons.length; i += 1) {
+  accordeons[i].addEventListener('click', function(event) {
+    const accordeonContent = accordeons[i].querySelector('.item-content');
+    const minusButton = accordeons[i].querySelector('.minus-button');
+    const accordeonTitle = accordeons[i].querySelector('.item-title');
+    const accordeonDivider = accordeons[i].querySelector('.divider');
+    accordeonContent.classList.toggle('visually-hidden');
+    if (accordeonContent.classList.contains('visually-hidden')) {
+      minusButton.style.backgroundImage = "url('../../assets/icons/accordeon-plus.svg')";
+      accordeonTitle.style.color = '#080029';
+      accordeonDivider.style.backgroundColor = '#d3d3d3';
+    } else {
+      minusButton.style.backgroundImage = "url('../../assets/icons/minus-button.svg')";
+      accordeonTitle.style.color = '#609fff';
+      accordeonDivider.style.backgroundColor = '#609fff';
+    }
+  })
+}
